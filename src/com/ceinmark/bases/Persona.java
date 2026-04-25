d6cccb1035f34cfdccc21a856b229b6422111a38
Ale <Ale@LAPTOP-1CJQIF30>
2026-04-25 22:27:01 +0200

    Corrección y ampliación de la clase Persona.

--- src/com/ceinmark/bases/Persona.java
+++ src/com/ceinmark/bases/Persona.java
@@ -1,37 +1,39 @@
 package com.ceinmark.bases;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Clase abstacta Persona.
  */
 public abstract class Persona {
 	private String nombre;
 	private String apellidos;
-	private char sexo;
+	private Character sexo;
 	private String dni;
-	private int edad;
+	private LocalDate fechaNacimiento;
 
 	/**
-	 * Constructor con parámetros
-	 * 
+	 * Constructor con parámetros.
 	 * @param nombre
 	 * @param apellidos
 	 * @param sexo
 	 * @param dni
 	 * @param edad
 	 */
-	public Persona(String nombre, String apellidos, char sexo, String dni, int edad) {
+	public Persona(String nombre, String apellidos, Character sexo, String dni, LocalDate fechaNacimiento) {			
 		this.nombre = nombre;
 		this.apellidos = apellidos;
 		this.sexo = sexo;
 		this.dni = dni;
-		this.edad = edad;
+		this.fechaNacimiento = fechaNacimiento;
 	}
 
 	/**
 	 * Constructor vacío
 	 */
 	public Persona() {
-		this("", "", ' ', "", 0);
+		this("", "", ' ', "", LocalDate.now().minusYears(17));
 	}
 
 	public String getNombre() {
@@ -50,6 +52,14 @@ public abstract class Persona {
 		this.apellidos = apellidos;
 	}
 
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(Character sexo) {
+		this.sexo = sexo;
+	}
+
 	public String getDni() {
 		return dni;
 	}
@@ -58,17 +68,41 @@ public abstract class Persona {
 		this.dni = dni;
 	}
 
-	public int getEdad() {
-		return edad;
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
 	}
 
-	public void setEdad(int edad) {
-		this.edad = edad;
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
 	}
 
+	/**
+	 * Método que devuelve un booleano en función del sexo de la persona para
+	 * facilitar el manejo de Strings.
+	 * @return {@code true} si la persona es mujer, {@code false} si la persona
+	 *         es un hombre.
+	 */
+	protected boolean chequeoSexo() {
+		boolean mujer = true;
+		if (sexo.equals('M') || sexo.equals('m')) {
+			mujer = true;
+		} else if (sexo.equals('H') || sexo.equals('h')) {
+			mujer = false;
+		}
+		return mujer;
+	}
+	
+	/**
+	 * Método abstracto que devuelve un String determinado en función del sexo de la
+	 * instancia, empleando el método chequeoSexo().
+	 */
+	public abstract String denominacionInstancia();
+	
 	@Override
 	public String toString() {
-		return "Datos de persona con DNI " + dni + ":\n\tNombre: " + nombre + ".\n\tApellidos: " + apellidos
-				+ ".\n\tSexo: " + sexo + ".\n\tEdad: " + edad + ".";
+		String preposicionDe = chequeoSexo() ? "de la " : "del ";
+		return "Datos " + preposicionDe + denominacionInstancia() + " con DNI " + dni + ":\n\tNombre: " + nombre
+				+ ".\n\tApellidos: " + apellidos + ".\n\tFecha de nacimiento: "
+				+ fechaNacimiento.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) + ".";
 	}
 }
