6f7c6ee3181d864e3a039fc3c9d0765250cc2bb7
DiegooDuck <diegooduck1121@gmail.com>
2026-04-29 09:02:41 +0200

    Clase de Alumnos que hereda de Persona

--- src/com/ceinmark/bases/Alumno.java
+++ src/com/ceinmark/bases/Alumno.java
@@ -1,5 +1,47 @@
 package com.ceinmark.bases;
 
-public class Alumno {
+import java.time.LocalDate;
 
+/*
+ Clase Alumno que hereda de Persona y utiliza el enum Materia.
+*/
+public class Alumno extends Persona {
+  
+
+   private double notaMedia;
+   /*
+    Constructor con parámetros
+    */
+   public Alumno(String nombre, String apellidos, char sexo, String dni, LocalDate fechaNacimiento, double notaMedia) {
+       super(nombre, apellidos, sexo, dni, fechaNacimiento);
+       this.notaMedia = notaMedia;
+   }
+   /**
+    * Constructor vacío
+    */
+   public Alumno() {
+       super();
+       this.notaMedia = 0.0;
+   }
+   // Getters y Setters
+  
+  
+ 
+   public double getNotaMedia() {
+	return notaMedia;
+}
+   public void setNotaMedia(double notaMedia) {
+	this.notaMedia = notaMedia;
+   }
+
+   @Override
+   public String toString() {
+       // Añadimos el toString a los datos del alumno
+       return super.toString() + ".\n\tNota media: " + notaMedia;
+   }
+   @Override
+   public String denominacionInstancia() {
+	// TODO Auto-generated method stub
+	return chequeoSexo() ? "Alumna" : "Alumno";
+   }
 }
